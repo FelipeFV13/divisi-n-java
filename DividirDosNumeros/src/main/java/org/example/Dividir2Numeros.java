04b862db04ec35e4d272a32051457aed6d900319
Felipe FV <feli13pe@gmail.com>
2024-08-04 18:29:31 -0500

    cambios en el codigo para que el usuario siga haciendo divisiones

--- DividirDosNumeros/src/main/java/org/example/Dividir2Numeros.java
+++ DividirDosNumeros/src/main/java/org/example/Dividir2Numeros.java
@@ -7,18 +7,33 @@ public class Dividir2Numeros {
 
     public static void dividir(){
        double num1, num2, resultado;
+       boolean nextDivi = false;
+       String userDesicion;
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Ingrese el pŕimer numero: ");
-        num1 = input.nextDouble();
+        do {
+            System.out.println("Ingrese el pŕimer numero: ");
+            num1 = input.nextDouble();
 
-        System.out.println("Ingrese al segundo numero: ");
-        num2 = input.nextDouble();
+            System.out.println("Ingrese al segundo numero: ");
+            num2 = input.nextDouble();
 
-        resultado = num1 / num2;
+            resultado = num1 / num2;
 
-        System.out.printf("El resultado es %.1f", resultado);
+            System.out.printf("El resultado es %.1f \n", resultado);
+
+            System.out.print("Quiere realizar otra división?: ");
+            userDesicion = input.next();
+
+            if(userDesicion.toLowerCase().equals("si")){
+                nextDivi = true;
+            }
+
+
+        }while (nextDivi);
+
+
     }
 
     public static void main(String[] args) {
